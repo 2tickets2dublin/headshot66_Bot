35343cdfb383a86d8d0d1e134f852edcab779119
2tickets2dublin <indasad@gmail.com>
2023-02-12 17:02:54 +0500

    init2

--- Git_bot/main.py
+++ Git_bot/main.py
@@ -1,26 +1,23 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import Message
 from config import TOKEN_API
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher()
 
 
-@dp.message(Command(commands=["start"]))
-async def cmd_start(message: types.Message):
-    await message.answer("<strong><em>Hello! You are welkome to our BOT!</em></strong>", parse_mode="HTML")
+@dp.message(Command(commands="start"))
+async def process_start_command(message: Message):
+    await message.answer("Hello nigger")
 
-
-@dp.message(Command(commands=["gives"]))
-async def start_command(message: types.Message):
-    await bot.send_sticker(message.from_user.id, sticker="CAACAgIAAxkBAAEHr-Nj56y2LCy6z4GHmv2Alq76izydjQACBQwAAqQp6Eva0HNoMuvnQi4E")
-    await message.delete()
-
+@dp.message(Command(commands="help"))
+async def process_help_command(message: Message):
+    await message.answer("Hello, its Help command")
 
 @dp.message()
-async def send_emo(message: types.Message):
-    await message.reply(message.text + " ❤️")
-
+async def send_echo(message: Message):
+    await message.reply(message.text)
 
 if __name__ == "__main__":
     dp.run_polling(bot)
